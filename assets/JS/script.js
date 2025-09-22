aca3bafe40964314153eefb3e7c69f3ea9d362e9
Rob-C-89 <robbiecornell@gmail.com>
2025-09-22 11:22:54 +0100

    Add fundamental JS variable and functions

---
+++ assets/JS/script.js
@@ -0,0 +1,20 @@
+// Game Scenarios data structure
+const gameScenarios = {
+}
+
+// Function to display scenario
+function displayScenario () {
+}
+
+// Function to make user choices
+function makeChoice() {
+}
+
+// Function to restart game 
+function restartGame() {
+}
+
+// Initialize game by displaying the first scenario
+displayScenario();
+
+
